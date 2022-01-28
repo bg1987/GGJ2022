95f3367f430a5b44bd620a38bfd4276028ede750
bg1987 <bg1987@gmail.com>
2022-01-29 01:32:27 +0200

    fixed rule generation and rule layout.

--- Assets/Scripts/LawFactory.cs
+++ Assets/Scripts/LawFactory.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LawFactory : MonoBehaviour
 {
     public TraitsCollection collection;
+    public GameObject ruleGroupPrefab;
+    public Transform mainContainer;
+    
     public int minTraits = 3;
     public int maxTraits = 6;
     public int maxRuleGroupSize = 3;    
@@ -21,14 +24,46 @@ public class LawFactory : MonoBehaviour
         create = false;
     }
 
+    
+    
     Law CreateLaw()
+    {
+        CleanContainer();
+        var law = Generate();
+        
+        foreach (var ruleGroup in law.rules)
+        {
+            InstantiateRuleGroup(ruleGroup);
+        }
+        // Canvas.ForceUpdateCanvases();
+        return law;
+    }
+
+    private void CleanContainer()
+    {
+        foreach (Transform child in mainContainer)
+        {
+            Debug.Log("Destroying");
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void InstantiateRuleGroup(RuleGroup ruleGroup)
+    {
+        Debug.Log("Creating rule group");
+        var newRule = Instantiate(ruleGroupPrefab,mainContainer,false);
+        var rc = newRule.GetComponent<RuleCanvas>();
+        rc.ApplyRule(ruleGroup);
+    }
+
+    private Law Generate()
     {
         var generatedGroups = new List<RuleGroup>();
 
         var selectedTraitsCount = Random.Range(minTraits, maxTraits + 1);
         var selectedTraits = new List<Trait>();
         var tempColl = new List<Trait>(collection.traits);
-        
+
         for (int i = 0; i < selectedTraitsCount; i++)
         {
             var selectedIndex = Random.Range(0, tempColl.Count);
@@ -50,23 +85,31 @@ public class LawFactory : MonoBehaviour
             //add the traits to the group
             for (int j = 0; j < groupSize; j++)
             {
-                var selectedIndex = Random.Range(1, selectedTraits.Count);
+                var selectedIndex = Random.Range(0, selectedTraits.Count);
                 group.traits[j] = selectedTraits[selectedIndex];
                 selectedTraits.RemoveAt(selectedIndex);
             }
+
             generatedGroups.Add(group);
         }
-        
+
         //all leftover traits are set into single groups
         for (int i = 0; i < selectedTraits.Count; i++)
         {
             var group = ScriptableObject.CreateInstance<RuleGroup>();
-            group.traits = new Trait[] { selectedTraits[i] };
-            generatedGroups.Add(group);
+            @group.traits = new Trait[] { selectedTraits[i] };
+            generatedGroups.Add(@group);
         }
 
         var law = ScriptableObject.CreateInstance<Law>();
         law.rules = generatedGroups.ToArray();
+
+        
+        Debug.Log("RuleGroups: " + law.rules.Length);
+        for (int i = 0; i < law.rules.Length; i++)
+        {
+            Debug.Log("Traits in group: "+ law.rules[i].traits.Length);
+        }
         
         return law;
     }
--- Assets/Scripts/RuleCanvas.cs
+++ Assets/Scripts/RuleCanvas.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RuleCanvas : MonoBehaviour
 {
     public GameObject traitPrefab;
 
-    public RectTransform traitCollection;
+    public Transform traitsContainer;
 
     public GameObject not;
 
     public void ApplyRule(RuleGroup rule)
     {
-        
+        not.SetActive(rule.not);
+        foreach (var trait in rule.traits)
+        {
+            var x = Instantiate(traitPrefab, traitsContainer.transform, false);
+            var traitImage = x.GetComponent<Image>();
+            traitImage.sprite = trait.image;
+        }
     }
 }
