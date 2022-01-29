10951ebaa62bd9c588af10b6fbcd31e7735067fb
bg1987 <bg1987@gmail.com>
2022-01-29 12:56:55 +0200

    progress takes and evaluates law on the entering soul

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -10,7 +10,7 @@ public class Queue : MonoBehaviour
 
     public static Queue Instance { get { return _instance; } }
 
-
+    public LawResolver resolver;
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -34,8 +34,9 @@ public class Queue : MonoBehaviour
         }
     }
 
-    public void Progress()
+    public bool Progress(Law currentLaw)
     {
+        var failedTest = false;
         for (int i = 0; i < slots.Count; i++)
         {
             if (slots[i].childCount > 0)
@@ -45,6 +46,15 @@ public class Queue : MonoBehaviour
                 {
                     currentSoul.transform.SetParent(entrance, false);
                     currentSoul.transform.GetChild(0).GetComponent<Animator>().Play("soul_enter", 0);
+                    failedTest = !resolver.EvaluateSoul(currentLaw, currentSoul.GetComponent<Soul>(), true);
+                    if (failedTest)
+                    {
+                        Debug.Log("HEAVEN WRONG");
+                    }
+                    else
+                    {
+                        Debug.Log("HEAVEN RIGHT");
+                    }
                 }
                 else
                 {
@@ -60,5 +70,7 @@ public class Queue : MonoBehaviour
                 }
             }
         }
+
+        return failedTest;
     }
 }
