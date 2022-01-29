9d76a8a3610d424124ffd7b2c4516ebcec4e8907
bg1987 <bg1987@gmail.com>
2022-01-29 17:17:28 +0200

    law resolver wip

--- Assets/Scripts/LawResolver.cs
+++ Assets/Scripts/LawResolver.cs
@@ -5,17 +5,63 @@ using UnityEngine;
 
 public class LawResolver : MonoBehaviour
 {
+    //true means a strike
     public bool EvaluateSoul(Law law, Soul soul, bool toHeaven)
+    {
+        bool soulPassed;
+        //required to enter heaven
+        if (law.heaven)
+        {
+            //sending to heaven
+            if (toHeaven)
+            {
+                soulPassed = MatchAnyRule(law, soul);
+            }
+            //sending to hell
+            else
+            {
+                //doesnt fit any rule to heaven
+                 soulPassed = !MatchAnyRule(law, soul);
+            }
+        }
+        else
+        {
+            if (toHeaven)
+            {
+                soulPassed = !MatchAnyRule(law, soul);
+            }
+            else
+            {
+                soulPassed = MatchAnyRule(law, soul);
+            }
+        }
+
+        return !soulPassed;
+    }
+
+    bool FailAllRules(Law law, Soul soul)
+    {
+        var result = true;
+        foreach (var rule in law.rules)
+        {
+            result = result && !EvaluateRule(rule, soul);
+        }
+
+        return result;
+    }
+
+    bool MatchAnyRule(Law law, Soul soul)
     {
         foreach (var rule in law.rules)
         {
-            if (EvaluateRule(rule, soul) && (toHeaven == law.heaven))
+            if (!EvaluateRule(rule, soul))
             {
-                return true;
+                return false;
             }
         }
-        return false;
-       }
+
+        return true;
+    }
 
     bool EvaluateRule(RuleGroup rules, Soul soul)
     {
